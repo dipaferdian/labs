832b69d0a3bb1257b6cdf47e56051929e6276815
dipaferdian <dipaferdian2@gmail.com>
2026-09-07 11:07:36 +0700

    valid anagram

--- languages/javascript.js
+++ languages/javascript.js
@@ -1,24 +1,34 @@
-function compute(nums) {
-  let currentSum = 0;
-  let maxSum = -Infinity;
+function compute(s, t) {
+  if (s.length != t.length) return false;
+  let map = new Map();
+  for (let index = 0; index < s.length; index++) {
+    const element = s[index];
 
-  for (let right = 0; right < nums.length; right++) {
-    currentSum += nums[right];
-    const currentNumber = nums[right];
-
-    if (currentNumber > currentSum) {
-      currentSum = currentNumber;
+    if (map.has(element)) {
+      const count = map.get(element) + 1;
+      map.set(element, count);
+    } else {
+      map.set(element, 1);
     }
-
-    maxSum = Math.max(maxSum, currentSum);
   }
 
-  return maxSum;
+  for (let index = 0; index < t.length; index++) {
+    const element = t[index];
+
+    if (map.get(element) >= 1) {
+      const decrement = map.get(element) - 1;
+
+      map.set(element, decrement);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log(compute([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
-console.log(compute([5, 2, -1, 0, 3]));
-console.log(compute([1]));
-console.log(compute([5, -1, 8]));
-console.log(compute([-2, 1, -3, 4]));
-console.log(compute([-2, -1]));
+console.log(compute("anagram", "nagaram"));
+console.log(compute("a", "aa"));
+console.log(compute("a", "ab"));
+console.log(compute("rat", "car"));
+console.log(compute("ab", "a"));
