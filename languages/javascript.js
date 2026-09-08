a9e3b63aafd51ae90837599588109358ecfc04da
dipaferdian <dipaferdian2@gmail.com>
2026-09-08 14:12:11 +0700

    two sum

--- languages/javascript.js
+++ languages/javascript.js
@@ -1,21 +1,20 @@
-function compute(nums) {
-  let set = new Set();
+function compute(nums, target) {
+  let map = new Map();
 
   for (let index = 0; index < nums.length; index++) {
     const element = nums[index];
 
-    if (set.has(element)) {
-      return element;
+    const missingNumber = target - element;
+
+    if (map.has(missingNumber)) {
+      return [map.get(missingNumber), index];
     } else {
-      set.add(element);
+      map.set(element, index);
     }
   }
 
   return null;
 }
 
-console.log(compute([1, 2, 2, 3, 1, 4, 2]));
-console.log(compute([5]));
-console.log(compute([1, 2, 3, 4, 5]));
-console.log(compute([1, 1]));
-console.log(compute([5, 5, 5, 5]));
+console.log(compute([2, 7, 11, 15], 9));
+console.log(compute([5], 5));
