97bf756eba40aedb944a923baa8af0c50aa8389b
dipaferdian <dipaferdian2@gmail.com>
2026-09-08 11:17:00 +0700

    Count Number Frequencies

--- languages/javascript.js
+++ languages/javascript.js
@@ -1,16 +1,19 @@
 function compute(nums) {
-  let min = Infinity;
+  let map = new Map();
 
   for (let index = 0; index < nums.length; index++) {
     const element = nums[index];
 
-    if (element < min) {
-      min = element;
+    if (map.has(element)) {
+      const count = map.get(element) + 1;
+      map.set(element, count);
+    } else {
+      map.set(element, 1);
     }
   }
 
-  return min;
+  return map;
 }
 
-console.log(compute([7, 3, 9, 2, 8, 4]));
-console.log(compute([-1, -5, -2, -10]));
+console.log(compute([1, 2, 2, 3, 1, 4, 2]));
+console.log(compute([5]));
