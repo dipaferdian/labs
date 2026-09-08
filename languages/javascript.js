15861a3a8e997beecfd03a47f3e4cc4a8c1b91f1
dipaferdian <dipaferdian2@gmail.com>
2026-09-08 16:12:00 +0700

    Maximum Subarray Sum

--- languages/javascript.js
+++ languages/javascript.js
@@ -1,16 +1,29 @@
-function compute(s) {
+function compute(nums, k) {
+  let windowSum = 0;
+  let maxNumber = -Infinity;
   let left = 0;
-  let right = s.length - 1;
 
-  while (left < right) {
-    if (s[left] != s[right]) return false;
-
-    left++;
-    right--;
+  for (let index = 0; index < k; index++) {
+    const element = nums[index];
+    windowSum += element;
   }
 
-  return true;
+  maxNumber = windowSum;
+
+  for (let index = k; index < nums.length; index++) {
+    const element = nums[index];
+
+    windowSum = windowSum - nums[left] + element;
+
+    left++;
+    maxNumber = Math.max(maxNumber, windowSum);
+  }
+
+  return maxNumber;
 }
 
-console.log(compute("katak"));
-console.log(compute("mobil"));
+console.log(compute([2, 1, 5, 1, 3, 2], 3));
+console.log(compute([1, 2, 3, 4, 5], 2));
+console.log(compute([5], 1));
+console.log(compute([1, 1, 1, 1], 2));
+console.log(compute([-1, -2, -3, -4], 2));
