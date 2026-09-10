874cf51d6173c5cae9c5aee6c78b9eacfaafd583
dipaferdian <dipaferdian2@gmail.com>
2026-09-10 17:11:44 +0700

    feat(dsa): find first unique number with frequency map

--- languages/javascript.js
+++ languages/javascript.js
@@ -1,5 +1,7 @@
 function compute(nums) {
   let map = new Map();
+  let maxCount = -Infinity;
+  let result = null;
 
   for (let index = 0; index < nums.length; index++) {
     const element = nums[index];
@@ -8,18 +10,30 @@ function compute(nums) {
       const count = map.get(element) + 1;
 
       map.set(element, count);
+
+      if (count > maxCount) {
+        maxCount = count;
+        result = element;
+      }
+
+      maxCount = Math.max(maxCount, count);
     } else {
       map.set(element, 1);
+
+      maxCount = Math.max(maxCount, 1);
     }
   }
 
-  for (let index = 0; index < nums.length; index++) {
-    const element = nums[index];
-
-    if (map.get(element) === 1) {
-      return element;
-    }
-  }
+  return result;
 }
 
-console.log(compute([1, 2, 3, 4, 1]));
+console.log(compute([1, 2, 2, 3, 1, 2]));
+// 2
+
+console.log(compute([5, 5, 1, 1, 1, 3]));
+// 1
+
+console.log(compute([7, 4, 7, 4]));
+// 7
+
+console.log(compute([1, 1]));
