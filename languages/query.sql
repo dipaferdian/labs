5f638caa109f72f1b9381afa5312bb675d801fa6
dipaferdian <dipaferdian2@gmail.com>
2026-09-17 11:05:05 +0700

    feat(sql): calculate average balance by category

--- languages/query.sql
+++ languages/query.sql
@@ -63,3 +63,14 @@ SELECT
   SUM(balance) as total_balance
 from accounts
 GROUP BY balance_group
+
+
+SELECT 
+  CASE
+      WHEN balance < 2000000 THEN 'LOW'
+      WHEN balance >= 2000000 THEN 'HIGH'
+      ELSE 'Zero'
+  END as balance_group,
+  AVG(balance) as average_balance
+from accounts
+GROUP BY balance_group
